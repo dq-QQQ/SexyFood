e22d18cb8ee0212ef336e9eda71e01376d21247c
dq-qqq <kjsl4tw@naver.com>
2025-06-26 20:13:46 +0900

    init

---
+++ SexyFood/SexyFood/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SexyFood
+//
+//  Created by Kyu jin Lee on 6/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SexyFood/SexyFood/SexyFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  SexyFoodApp.swift
+//  SexyFood
+//
+//  Created by Kyu jin Lee on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct SexyFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
