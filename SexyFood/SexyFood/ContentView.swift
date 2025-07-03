35c80272e0f6652142fe03dd9fbd3c2e1403205a
dq-qqq <kjsl4tw@naver.com>
2025-07-03 19:01:30 +0900

    init

--- SexyFood/SexyFood/ContentView.swift
+++ SexyFood/SexyFood/ContentView.swift
@@ -9,13 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Spacer()
+            HStack(spacing: 20) {
+                Spacer()
+                Button("베스트샷") {
+                    print("버튼 1 눌림")
+                }
+                Spacer()
+                Button("달력일기") {
+                    print("버튼 2 눌림")
+                }
+                Spacer()
+            }
+            Spacer()
+            HStack(spacing: 20) {
+                Spacer()
+                Button("알림트리거") {
+                    print("버튼 3 눌림")
+                }
+                Spacer()
+                Button("사진일기저장") {
+                    print("버튼 4 눌림")
+                }
+                Spacer()
+            }
+            Spacer()
+            HStack(spacing: 20) {
+                Spacer()
+                Button("버튼 5") {
+                    print("버튼 5 눌림")
+                }
+                Spacer()
+                Button("버튼 6") {
+                    print("버튼 6 눌림")
+                }
+                Spacer()
+            }
+            Spacer()
         }
         .padding()
+        .buttonStyle(.borderedProminent)
+        
     }
 }
 
---
+++ SexyFood/SexyFood/Func1.swift
@@ -0,0 +1,8 @@
+//
+//  Func1.swift
+//  SexyFood
+//
+//  Created by Kyu jin Lee on 6/26/25.
+//
+
+import Foundation
